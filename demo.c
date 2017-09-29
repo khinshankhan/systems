c01ebbfc22774ee067c146f7a0b9ea034d1816a7
Khinshan <kkhan@stuy.edu>
2017-09-28 21:02:57 -0400

    mini demo completed

--- demo.c
+++ demo.c
@@ -2,13 +2,25 @@
 #include <string.h>
 // strcmp vs strncmp lesson
 
-void print(int x){
-  printf("%d\n", x);
+void print11(int x){
+  printf("str1 compared to str1 is: %d\n", x);
 }
+void print12(int x){
+  printf("str1 compared to str2 is: %d\n", x);
+}
+void print21(int x){
+  printf("str2 compared to str1 is: %d\n", x);
+}
+void print23(int x){
+  printf("str2 compared to str3 is: %d\n", x);
+}
+
+
 
 int main(){
   //Remember the compareTo from Java? Same concept with strcmp
   //takes in two strings
+  //compares both strings lexicographically
   //short circuits as soon as there is an inequality
   
   //if first input is less than second, returns a number < 0
@@ -18,32 +30,34 @@ int main(){
   char str1[]= "Apple";
   char str2[]= "apple";
   char str3[] = "appLe";
+
+  printf("Please note:\nstr1[]= \"Apple\"\nstr2[]= \"apple\"\nstr3[] = \"appLe\"\n");
   
-  printf("strcmp demo:\n");
+  printf("\nstrcmp demo:\n");
   value = strcmp(str1, str2);
-  print(value);
+  print12(value);
   value = strcmp(str2, str1);
-  print(value);
+  print21(value);
   value = strcmp(str1, str1);
-  print(value);
-  prinf("Clincher: ");
+  print11(value);
   value = strcmp(str2, str3);
-  print(value);
+  print23(value);
 
   //strncmp has a similar concept, but it takes in one more input (total: 3)
   // the third is the maximum number of characters to compare up to
 
-  printf("strncmp demo:\n");
+  printf("\nstrncmp demo:\n");
   value = strncmp(str1, str2, 5);
-  print(value);
+  print12(value);
   value = strncmp(str2, str1, 5);
-  print(value);
+  print21(value);
   value = strncmp(str1, str1, 5);
-  print(value);
+  print11(value);
   value = strncmp(str2, str3, 5);
-  print(value);
+  print23(value);
+  printf("\nClincher (only up to 3 letters): ");
   value = strncmp(str2, str3, 3);
-  print(value);
+  print23(value);
 
 
   return 0;
