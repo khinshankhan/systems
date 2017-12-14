9a2c24f2726b78a4267d1475197c1dd9624d80c5
Khinshan Khan <kkhan@stuy.edu>
2017-12-14 15:39:24 -0500

    set up to accept multiple clients

--- basic_server.c
+++ basic_server.c
@@ -17,14 +17,25 @@ int main() {
   int from_client;
 
   char buf[1024];
+  int status;
 
-  from_client = server_handshake( &to_client );
+  //loop allows for other clients to connect later
+  while(1){
+    int f = fork();
+    if(f){
+      wait(&status);
+      printf("\n\n\n");
+    }
+    else{
+      from_client = server_handshake( &to_client );
 
-  while (1) {
-    read(from_client, buf, sizeof(buf));
-    printf("Received a message: %s\n", buf);
-    reverse(buf);
-    write(to_client, buf, sizeof(buf));
+      while (1) {
+	read(from_client, buf, sizeof(buf));
+	printf("Received a message: %s\n", buf);
+	reverse(buf);
+	write(to_client, buf, sizeof(buf));
+      }
+    }
   }
 
   return 0;
