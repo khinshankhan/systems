3a682fb3b6ae68fe1c3da8120d4e0a85464a862a
Khinshan Khan <kkhan@stuy.edu>
2017-12-19 23:50:02 -0500

    i think im done with pipe_networking

--- pipe_networking.c
+++ pipe_networking.c
@@ -17,11 +17,22 @@ int server_setup() {
 
   printf(WKP);
 
-  mkfifo(WKP, 0600);
+  int make_status = mkfifo(WKP, 0600);
+  //in case pipe couldnt be made
+  if(make_status == -1){
+    printf("Error: WKP Creation has failed. Billy Mays is not liable.\n");
+    exit(1);
+  }
 
   //block on open, recieve mesage
   printf("[server] handshake: making wkp\n");
   up_pipe = open(WKP, O_RDONLY, 0);
+  //in case pipe couldnt be accessed
+  if(up_pipe == -1) {
+    printf("Error: WKP Access has failed. Billy Mays is not reliable.\n");
+    remove(WKP);
+    exit(1);
+  }
   read(up_pipe, buffer, sizeof(buffer));
   printf("[server] handshake: received [%s]\n", buffer);
 
@@ -45,28 +56,49 @@ int server_connect(int from_client) {
 
   //send pp name to server
   printf("[client] handshake: connecting to wkp\n");
-  *to_server = open(WKP, O_WRONLY, 0);
-  if ( *to_server == -1 )
+  dwn_pipe = open(WKP, O_WRONLY, 0);
+  if(dwn_pipe == -1 ){
+    printf("Error: Billy Mays isn't finding his paparazzi\n");
+    //remove(from_client);
     exit(1);
+  }
 
   //make private pipe
   sprintf(buffer, "%d", getpid() );
-  mkfifo(buffer, 0600);
+  int make_status = mkfifo(buffer, 0600);
+  //in case pipe couldnt be made
+  if(make_status == -1){
+    printf("Error: Local Pipe Creation has failed. This is not part of Billy Mays' job.\n");
+    exit(1);
+  }
 
-  write(*to_server, buffer, sizeof(buffer));
+  make_status = write(dwn_pipe, buffer, sizeof(buffer));
+  if(make_status == -1){
+    printf("Error: Local Pipe Writing has failed. This is not part of Billy Mays' job.\n");
+    exit(1);
+  }
 
   //open and wait for connection
-  from_server = open(buffer, O_RDONLY, 0);
-  read(from_server, buffer, sizeof(buffer));
+  dwn_pipe = open(buffer, O_RDONLY, 0);
+  make_status = read(dwn_pipe, buffer, sizeof(buffer));
+  if(make_status == -1){
+    printf("Error: Local Pipe Reading has failed. This is not part of Billy Mays' job.\n");
+    exit(1);
+  }
   /*validate buffer code goes here */
-  printf("[client] handshake: received [%s]\n", buffer);
+  if(strncmp(buffer, ACK, sizeof(buffer)) == 0) {
+    printf("[client] handshake: received [%s]\n", buffer);
+  }
+  else{
+    printf("Error: received message \"%s\" instead of confirmation message \"%s\".\nBilly Mays' instructions were not listened to\n", buffer, ACK);
+  }
 
   //remove pp
   remove(buffer);
   printf("[client] handshake: removed pp\n");
 
   //send ACK to server
-  write(*to_server, ACK, sizeof(buffer));
+  //write(*to_server, ACK, sizeof(buffer));
 
   return dwn_pipe;
 }
@@ -86,12 +118,29 @@ int server_handshake(int *to_client) {
 
   char buffer[HANDSHAKE_BUFFER_SIZE];
 
-  mkfifo(WKP, 0600);
+  int make_status = mkfifo(WKP, 0600);
+  //in case pipe couldnt be made
+  if(make_status == -1){
+    printf("Error: WKP Creation has failed. Billy Mays is not liable.\n");
+    exit(1);
+  }
 
   //block on open, recieve mesage
   printf("[server] handshake: making wkp\n");
   from_client = open(WKP, O_RDONLY, 0);
-  read(from_client, buffer, sizeof(buffer));
+  if(from_client == -1){
+    printf("Error: Local Pipe opening has failed. Billy Mays' client doesn't exist. He didn't fail at anything.\n");
+    //remove(WKP);
+    exit(1);
+  }
+
+  make_status = read(from_client, buffer, sizeof(buffer));
+  if(make_status == -1){
+    printf("Error: Local Pipe reading has failed. Billy Mays' eyesight may be declining.\n");
+    //remove(WKP);
+    exit(1);
+  }
+
   printf("[server] handshake: received [%s]\n", buffer);
 
   remove(WKP);
